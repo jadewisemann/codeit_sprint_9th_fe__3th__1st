aa3a113758ccb2e80c6b2b2524030832d9f38fc7
Seokyung <xuejd17@gmail.com>
2025-05-15 00:29:45 +0900

    확인용으로 작성한 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,3 @@
-import Tag from '@/components/Tag';
-
-const Home = () => (
-  <div className='m-3 space-x-2'>
-    <Tag>태그1</Tag>
-    <Tag variant='sm'>태그2</Tag>
-    <Tag variant='md' className='bg-blue-200'>
-      태그3
-    </Tag>
-    <Tag variant='lg' className='border' isRemovable>
-      태그4
-    </Tag>
-  </div>
-);
-
-export default Home;
+export default function Home() {
+  return <div>hello</div>;
+}
