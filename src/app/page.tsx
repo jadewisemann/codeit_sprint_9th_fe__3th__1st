cf4c7a0825580b28fd4abf1af5bcbdf7d4ef6c26
Seokyung <xuejd17@gmail.com>
2025-05-15 00:26:55 +0900

    Tag 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-  return <div>hello</div>;
-}
+import Tag from '@/components/Tag';
+
+const Home = () => (
+  <div className='m-3 space-x-2'>
+    <Tag>태그1</Tag>
+    <Tag variant='sm'>태그2</Tag>
+    <Tag variant='md' className='bg-blue-200'>
+      태그3
+    </Tag>
+    <Tag variant='lg' className='border' isRemovable>
+      태그4
+    </Tag>
+  </div>
+);
+
+export default Home;
